ed06dedd8f6d07b6c453838644eb1396a1189d30
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-10-24 16:25:51 +0330

    Initial implementation of DataAccessLayer + tests

---
+++ src/PKG/DataAccessLayer.cpp
@@ -0,0 +1,101 @@
+//
+// Created by Reza Alizadeh Majd on 10/21/19.
+//
+
+#include "DataAccessLayer.h"
+
+#include <QDir>
+#include <QDebug>
+#include <utility>
+
+// TextSearchQuery
+namespace PKG {
+    TextSearchQuery::TextSearchQuery(QString keyword) : m_keyword(std::move(keyword)) {}
+
+    QString TextSearchQuery::query() const {
+        return QString("(title ~ '%1' || description ~ '%1')")
+                .arg(m_keyword);
+    }
+}
+
+// PackageNameSearchQuery
+namespace PKG {
+    PackageNameSearchQuery::PackageNameSearchQuery(QString pkgName) : m_packageName(std::move(pkgName)) {}
+
+    QString PackageNameSearchQuery::query() const {
+        return QString("(name = '%1')").arg(m_packageName);
+    }
+}
+
+// CategorySearchQuery
+namespace PKG {
+    CategorySearchQuery::CategorySearchQuery(QString categoryName) : m_category(std::move(categoryName)) {}
+
+    QString CategorySearchQuery::query() const {
+        return QString("(category = '%1')")
+                .arg(m_category);
+    }
+}
+
+// DataAccessLayer
+namespace PKG {
+    DataAccessLayer::DataAccessLayer(const QString &dbBasePath) :
+            m_categoryDB(QString("%1/category.rec").arg(dbBasePath)),
+            m_packageDB(QString("%1/packages/").arg(dbBasePath)) {}
+
+    QVector<Category *> DataAccessLayer::categoryList() {
+        QVector<Category *> result;
+        auto rawRecords = m_categoryDB.find();
+        for (const auto &rec : rawRecords) {
+            auto *cat = Category::MakeCategory(rec, this);
+            if (cat != nullptr) {
+                result.append(cat);
+            }
+        }
+        return result;
+    }
+
+    QVector<Package *> DataAccessLayer::findPackages(const QString &keyword) {
+        QVector<SearchQueryBase *> query;
+        query.append(new TextSearchQuery(keyword));
+        auto result = this->performPackageSearch(query);
+        qDeleteAll(query);
+        return result;
+    }
+
+    QVector<Package *> DataAccessLayer::categoryPackages(const QString &categoryName) {
+        QVector<SearchQueryBase *> query;
+        query.append(new CategorySearchQuery(categoryName));
+        auto result = this->performPackageSearch(query);
+        qDeleteAll(query);
+        return result;
+    }
+
+    Package *DataAccessLayer::packageDetails(const QString &packageName) {
+        QVector<SearchQueryBase *> query;
+        query.append(new PackageNameSearchQuery(packageName));
+        auto result = this->performPackageSearch(query);
+        qDeleteAll(query);
+        return result.empty() ? nullptr : result[0];
+    }
+
+    QVector<Package *> DataAccessLayer::performPackageSearch(const QVector<SearchQueryBase *> &filters) {
+        QVector<Package *> result;
+        QString queryStr;
+        for (const auto &filter : filters) {
+            if (!queryStr.isEmpty()) {
+                queryStr.append(" && ");
+            }
+            queryStr.append(filter->query());
+        }
+        auto recs = m_packageDB.find(queryStr);
+        for (const auto &rec : recs) {
+            auto *pkg = Package::MakePackage(rec, this);
+            if (pkg != nullptr) {
+                result.append(pkg);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/PKG/DataEntities.h
@@ -0,0 +1,118 @@
+//
+// Created by Reza Alizadeh Majd on 10/23/19.
+//
+
+#ifndef PX_SOFTWARE_DATAENTITIES_H
+#define PX_SOFTWARE_DATAENTITIES_H
+
+#include <QObject>
+#include <QVariant>
+#include "../RecDB.h"
+
+namespace PKG {
+
+    class DataAccessLayer;
+
+    class BaseEntity : public QObject {
+        Q_OBJECT
+    public:
+        explicit BaseEntity(QObject *parent = nullptr);
+
+        bool checkAndSetProperty(const QString &propertyName,
+                                 const RecRecord &record,
+                                 bool isList = false,
+                                 const QString &sourceKey = QString());
+    };
+
+
+    class Category : public BaseEntity {
+        Q_OBJECT
+        Q_PROPERTY(QString name READ name WRITE setName)
+        Q_PROPERTY(QString title READ title WRITE setTitle)
+        Q_PROPERTY(QString icon READ icon WRITE setIcon)
+        Q_PROPERTY(QString description READ description WRITE setDescription)
+    protected:
+        explicit Category(QObject *parent = nullptr);
+
+    public:
+        static Category* MakeCategory(const RecRecord &rec, QObject *parent);
+
+        QString name() const { return m_name; }
+        void setName(const QString &value) { m_name = value; }
+
+        QString title() const { return m_title; }
+        void setTitle(const QString &value) { m_title = value; }
+
+        QString icon() const { return m_icon; }
+        void setIcon(const QString &value) { m_icon = value; }
+
+        QString description() const { return m_description; }
+        void setDescription(const QString &value) { m_description = value; }
+
+    private:
+        QString m_name;
+        QString m_title;
+        QString m_icon;
+        QString m_description;
+    };
+
+
+    class Package : public BaseEntity {
+        Q_OBJECT
+        Q_PROPERTY(QString name READ name WRITE setName)
+        Q_PROPERTY(QString title READ title WRITE setTitle)
+        Q_PROPERTY(QString version READ version WRITE setVersion)
+        Q_PROPERTY(QString description READ description WRITE setDescription)
+        Q_PROPERTY(QString homepage READ homePage WRITE setHomePage)
+        Q_PROPERTY(QString license READ license WRITE setLicense)
+        Q_PROPERTY(QString icon READ icon WRITE setIcon)
+        Q_PROPERTY(QStringList screenshots READ screenShots WRITE setScreenShots)
+        Q_PROPERTY(QStringList categories READ categories WRITE setCategories)
+    protected:
+        explicit Package(QObject *parent);
+
+    public:
+        static Package* MakePackage(const RecRecord &rec, QObject *parent);
+
+        QString name() const { return m_name; }
+        void setName(const QString &value) { m_name = value; }
+
+        QString title() const { return m_title; }
+        void setTitle(const QString &value) { m_title = value; }
+
+        QString version() const { return m_version; }
+        void setVersion(const QString &value) { m_version = value; }
+
+        QString description() const { return m_description; }
+        void setDescription(const QString &value) { m_description = value; }
+
+        QString homePage() const { return m_homePage; }
+        void setHomePage(const QString &value) { m_homePage = value; }
+
+        QString license() const { return m_license; }
+        void setLicense(const QString &value) { m_license = value; }
+
+        QString icon() const { return m_icon; }
+        void setIcon(const QString &value) { m_icon = value; }
+
+        QStringList screenShots() const { return m_screenshots; }
+        void setScreenShots(const QStringList &value) { m_screenshots = value; }
+
+        QStringList categories() const { return m_categories; }
+        void setCategories(const QStringList &value) { m_categories = value; }
+
+    private:
+        QString m_name;
+        QString m_title;
+        QString m_version;
+        QString m_description;
+        QString m_homePage;
+        QString m_license;
+
+        QString m_icon;
+        QStringList m_screenshots;
+        QStringList m_categories;
+    };
+}
+
+#endif //PX_SOFTWARE_DATAENTITIES_H
