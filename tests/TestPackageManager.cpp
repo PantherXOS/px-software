f59cf4a09130dccf01357f132ed179b8d7a982dc
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-11-08 23:58:42 +0330

    PackageManager: write test for install package

--- tests/TestPackageManager.cpp
+++ tests/TestPackageManager.cpp
@@ -4,6 +4,7 @@
 
 #include <QtTest/QtTest>
 #include <PKG/PackageManager.h>
+#include <QDebug>
 
 using namespace PKG;
 
@@ -13,14 +14,26 @@ public:
     explicit TestPackageManager(QObject *parent = nullptr);
 
 private slots:
+    void initTestCase();
     void getInstalledPackages();
     void getUserUpgradablePackages();
     void getSystemUpgradablePackages();
+    void installPackage();
 
 private:
     QString m_dbPath = "./SAMPLE_DB";
     PackageManager *m_pkgMgr = nullptr;
 };
+
+void TestPackageManager::initTestCase() {
+    // cleanup environment for test execution
+
+    // 1. uninstall `hello` package
+    QProcess process(this);
+    process.start("guix", QStringList() << "package" << "-r" << "hello");
+    process.waitForStarted();
+    process.waitForFinished(60000);
+}
 
 TestPackageManager::TestPackageManager(QObject *parent) : QObject(parent) {
     m_pkgMgr = new PackageManager(m_dbPath, this);
@@ -51,6 +64,27 @@ void TestPackageManager::getSystemUpgradablePackages() {
     while (!spy.wait() && !spyError.wait()) {}
     QCOMPARE(spy.count(), 1);
     QCOMPARE(spyError.count(), 0);
+}
+
+void TestPackageManager::installPackage() {
+    QString packageName = "hello";
+    QSignalSpy spy(m_pkgMgr, &PackageManager::packageInstalled);
+    QSignalSpy spyError(m_pkgMgr, &PackageManager::failed);
+    QSignalSpy spyInstallLog(m_pkgMgr, &PackageManager::newTaskData);
+    QObject::connect(m_pkgMgr, &PackageManager::newTaskData,
+                     [&](const QUuid &taskId, const QString &data) {
+                         qDebug() << QString("new data for %1: %2")
+                                 .arg(taskId.toString())
+                                 .arg(data);
+                     });
+    m_pkgMgr->requestPackageInstallation(packageName);
+    while (!(spy.count() > 0
+             || spy.wait()
+             || spyError.count() > 0
+             || spyError.wait())) {}
+    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spyError.count(), 0);
+    QVERIFY(spyInstallLog.count() > 0);
 }
 
 QTEST_GUILESS_MAIN(TestPackageManager)
