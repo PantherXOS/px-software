1574221ca30c7f88654768c1a6e16d6082a0e648
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-11-08 22:51:02 +0330

    update AsyncTaskRunner
    - add signal to report process output
    - add Id field

--- src/AsyncTaskRunner.h
+++ src/AsyncTaskRunner.h
@@ -7,6 +7,7 @@
 
 #include <QObject>
 #include <QProcess>
+#include <QUuid>
 
 class AsyncTaskRunner : public QObject {
     Q_OBJECT
@@ -15,16 +16,21 @@ public:
     bool asyncRun(const QString &appName, const QStringList &args = QStringList());
     bool wait();
 
+    QUuid Id() const { return m_id; }
     int timeout() const { return m_timeout; }
     void setTimeout(int timeout) { m_timeout = timeout; }
 
 signals:
-    void done(const QString &stdOut, const QString &stdError);
-    void failed(const QString &stdOut, const QString &stdError);
+    void done(const QString &outData, const QString &errData);
+    void failed(const QString &message);
+    void newData(const QString &outData, const QString &errData);
 
 private:
+    QUuid m_id;
     QProcess m_worker;
     int m_timeout = 30000;
+    QString m_outStr;
+    QString m_errStr;
 };
 
 #endif //PX_SOFTWARE_TASKRUNNER_H
