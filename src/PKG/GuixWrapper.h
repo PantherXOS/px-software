8b4fdd5ae5d35aa0e69be0a079b0363119d5d32b
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-12-01 23:30:35 +0330

    PackageManager: GuixWrapper implemented
    GuixWrapper: execute guix related tasks sequentially, to prevent collision.

---
+++ src/PKG/GuixWrapper.h
@@ -0,0 +1,38 @@
+//
+// Created by Reza Alizadeh Majd on 12/1/19.
+//
+
+#ifndef PX_SOFTWARE_GUIXWRAPPER_H
+#define PX_SOFTWARE_GUIXWRAPPER_H
+
+#include <QObject>
+#include <QTimer>
+#include <QVector>
+#include <QMutex>
+#include <QMutexLocker>
+#include "GUIX/GuixTask.h"
+
+namespace PKG {
+    class GuixWrapper : public QObject {
+        Q_OBJECT
+    public:
+        explicit GuixWrapper(QObject *parent = nullptr);
+        void start();
+        void stop();
+
+    public slots:
+        bool appendTask(GuixTask *task, bool force = false);
+        bool cancelTask(const QUuid &taskId);
+
+    protected slots:
+        void execNextTask();
+
+    private:
+        QTimer m_timer;
+        QMutex m_lock;
+        GuixTask *m_currentTask;
+        QVector<GuixTask *> m_taskQueue;
+    };
+}
+
+#endif //PX_SOFTWARE_GUIXWRAPPER_H
