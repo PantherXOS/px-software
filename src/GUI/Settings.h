9e3e7a532fe52cd3e7cb213d6fdcbbc74b27c383
Hamzeh <h.nasajpour@pantherx.org>
2020-01-21 00:15:25 +0330

    Update searchbox color-scheme based on current color-scheme with QPalette.

--- src/GUI/Settings.h
+++ src/GUI/Settings.h
@@ -23,7 +23,7 @@
 #define SEARCH_BAR_FONT_SIZE            12
 #define SEARCH_BAR_FONT_COLOR           "#474646"
 #define SEARCH_BAR_BG_COLOR             "transparent"
-//#define SEARCH_BAR_STYLE                "QLineEdit {}"
+#define SEARCH_BAR_STYLE                "QLineEdit {background-color: %1; color: %2;}"
 
 #define CONTENT_LIST_ITEM_W             250
 #define CONTENT_LIST_ULINE_W            CONTENT_LIST_ITEM_W-10
