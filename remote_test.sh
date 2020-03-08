38c7967de408d8183a01cb6341a99e5b98b201f7
Reza Alizadeh Majd <r.majd@pantherx.org>
2020-03-08 18:34:31 +0330

    updae remote test script

--- remote_test.sh
+++ remote_test.sh
@@ -8,7 +8,7 @@ SERVER_ADDRESS="panther@127.0.0.1"
 
 ssh ${SERVER_ADDRESS} -p 2222 "mkdir -p $TARGET_PATH"
 
-rsync -av -e "ssh -p 2222" --exclude ".git" --exclude "cmake-build-debug" --exclude "docs/html" --exclude ".idea" "." "$SERVER_ADDRESS:$TARGET_PATH/"
+rsync -av -e "ssh -p 2222" --exclude ".git" --exclude "cmake-build-debug" --exclude "build" --exclude "docs/html" --exclude ".idea" "." "$SERVER_ADDRESS:$TARGET_PATH/"
 
 CMD='export GUILE_LOAD_PATH=$GUILE_LOAD_PATH:.'
 CMD="$CMD;
